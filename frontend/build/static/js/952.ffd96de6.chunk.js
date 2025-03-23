2cca8d344d20e81b1d7b21f82b4ecf74572404ba
Aman Shrivastava <aman34503@gmail.com>
2025-03-23 16:26:58 +0530

    build commit

---
+++ frontend/build/static/js/952.ffd96de6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkresume_to_questions=self.webpackChunkresume_to_questions||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.ffd96de6.chunk.js.map
